ce3072938392b9739d5dd8648c75e743e41e2f74
upovibe <uzorpromiseokwudiri@gmail.com>
2025-01-05 23:33:49 +0000

    update

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -5,15 +5,9 @@ import { useAuth } from "@/contexts/AuthContext";
 import Loading from "@/components/ui/Loading";
 import Toolbar from '@/components/layouts/Toolbar';
 import FirstPollCTA from '@/components/layouts/FirstPollCTA';
-import { useRouter } from "next/navigation"; 
 
 const DashboardPage = () => {
   const { loading } = useAuth();
-  const router = useRouter(); 
-
-  const handleAddNewPoll = () => {
-    router.push("/dashboard/polls/new");
-  };
 
   if (loading) {
     return <Loading />;
@@ -22,7 +16,7 @@ const DashboardPage = () => {
   return (
     <div className='p-4'>
       <Toolbar/>
-      <FirstPollCTA onCreatePoll={handleAddNewPoll}/>
+      <FirstPollCTA/>
     </div>
   );
 }
